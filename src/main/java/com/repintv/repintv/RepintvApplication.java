edcc4f15c07a40fa8e4c9c45aa25fa7563e09f07
andirhl <andi@ppu.co.id>
2024-02-29 08:11:06 +0700

    first commit

---
+++ src/main/java/com/repintv/repintv/RepintvApplication.java
@@ -0,0 +1,13 @@
+package com.repintv.repintv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepintvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepintvApplication.class, args);
+	}
+
+}
